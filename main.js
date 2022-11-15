9b7ef3c1215d34322251d8e412af99bf2fc57400
Julian <lc160518@ll.liemerscollege.nl>
2022-11-15 22:58:36 +0100

    my branch i lost

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 function rps() {
     var me = document.getElementById("input").value;
 
+    const options["rock", "scissors", "paper"];
+    const randomElement = array[Math.floor(Math.random() * array.length)];
+
+    const random = Math.floor(Math.random() * options.length);
+
+
     if(me == "scissors"){
-        document.getElementById("result").innerHTML = ("rock")
+        document.getElementById("result").innerHTML = (options[random])
         return;
     }
 
     if(me == "rock"){
-    document.getElementById("result").innerHTML = ("paper")
+    document.getElementById("result").innerHTML = (options[random])
     return;
     }
 
     if(me == "paper"){
-        document.getElementById("result").innerHTML = ("scissors")
+        document.getElementById("result").innerHTML = (options[random])
         return;
     }
 
